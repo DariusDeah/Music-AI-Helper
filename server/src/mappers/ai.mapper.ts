e442b7ca45f22c5d3af53a3fbeed318e204b30e4
Darius Deah <Dariusdeah21@gmail.com>
2024-01-18 16:27:34 -0700

    Add AI entity mapper

---
+++ server/src/mappers/ai.mapper.ts
@@ -0,0 +1,7 @@
+import { ChatCompletion } from "openai/resources";
+
+export class AIMapper {
+  static toChatMessageResponse(data: ChatCompletion) {
+    return data.choices[0].message.content;
+  }
+}
